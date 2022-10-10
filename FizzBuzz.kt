bccb01d9d19df25bf78f7886afb40655e8559152
JoseD <112482482+JoseD97@users.noreply.github.com>
2022-10-10 23:03:52 +0200

    Update FizzBuzz.kt

--- FizzBuzz.kt
+++ FizzBuzz.kt
@@ -1,17 +1,10 @@
-fun main(args: Array<String>){
+fun main(){
 
-    for(i in 0..100){
-        if(i % 3 == 0){
-            println("Fizz")
-        }
-        if(i % 5 == 0){
-            println("Buzz")
-        }
-        if(i % 3 == 0 && i % 5 == 0){
-            println("FizzBuzz")
-        }
-        else {
-            println(i)
-        }
+    for(i in 1..100){
+        if(i % 3 == 0 && i % 5 == 0) println("FizzBuzz")
+        else if(i % 3 == 0) println("Fizz")
+        else if(i % 5 == 0) println("Buzz")
+        else println(i)
     }
+
 }
